508b33c94d84995ca4367cfc212699557ce0c1c5
unknown <deyoucyril35@gmail.com>
2025-09-21 19:51:35 +0200

    config.example added

--- admin/header.php
+++ admin/header.php
@@ -59,7 +59,7 @@ $allowed_pages_for_seller = array('product.php','product-add.php','product-edit.
 		<header class="main-header">
 
 			<a href="index.php" class="logo">
-				<span class="logo-lg">eCommerce PHP</span>
+				<span class="logo-lg">Alexdy</span>
 			</a>
 
 			<nav class="navbar navbar-static-top">
